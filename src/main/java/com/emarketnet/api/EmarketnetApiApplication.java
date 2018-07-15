2e8d69f7f91043e2957b76528f8ca0b911d49d06
ilyav <ivald79@gmail.com>
2018-07-15 10:36:38 -0400

    create initial api aplication

---
+++ src/main/java/com/emarketnet/api/EmarketnetApiApplication.java
@@ -0,0 +1,12 @@
+package com.emarketnet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmarketnetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmarketnetApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/emarketnet/api/EmarketnetApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emarketnet.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmarketnetApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
